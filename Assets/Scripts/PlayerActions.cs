39221ef1e68e5bfe7b44da05a4793475d4fa90c2
resh7007 <resh87@mail.ru>
2022-08-27 19:42:27 +0600

    added  punching

--- Assets/Scripts/PlayerActions.cs
+++ Assets/Scripts/PlayerActions.cs
@@ -1,14 +1,29 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerActions : MonoBehaviour
 {
+    private Animator Anim;
+
     [SerializeField] private float JumpSpeed;
     [SerializeField] private GameObject Player1;
-    
 
-  
+    void Start()
+    {
+        Anim = GetComponent<Animator>();
+        
+    }
+
+    private void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Anim.SetTrigger("LightPunch");
+        }
+    }
+
     public void JumpUp()
     {
         Player1.transform.Translate(0,JumpSpeed,0);
