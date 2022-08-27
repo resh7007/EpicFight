1d042fadd8f1961834d6fc4516dab855449de699
resh7007 <resh87@mail.ru>
2022-08-27 15:29:47 +0600

    added mouse movement

--- Assets/Scripts/Player1Move.cs
+++ Assets/Scripts/Player1Move.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 public class Player1Move : MonoBehaviour
 {
     private Animator Anim;
+
+    [SerializeField] private float WalkSpeed;
     // Start is called before the first frame update
     void Start()
     {
-        Anim = GetComponent<Animator>();
+        Anim = GetComponentInChildren<Animator>();
         
     }
 
@@ -25,10 +27,13 @@ public class Player1Move : MonoBehaviour
         if (Input.GetAxis("Horizontal")>0)
         {
             Anim.SetBool("Forward",true);
+            transform.Translate(WalkSpeed,0,0);
         }
         if (Input.GetAxis("Horizontal")<0)
         {
             Anim.SetBool("Backward",true);
+            transform.Translate(-WalkSpeed,0,0);
+
         }
         if (Input.GetAxis("Horizontal")==0)
         {
